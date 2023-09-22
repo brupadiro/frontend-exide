6127db16db59d1f822e56f0aa15df5516d0c2753
brupadiro <brunodiharce@gmail.com>
2023-09-22 03:49:16 -0300

    cambio nombre modulo

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
 
   head: {
-    titleTemplate: 'Exide',
-    title: 'Exide',
+    titleTemplate: 'Exide Modulo quimico',
+    title: 'Exide Modulo quimico',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
